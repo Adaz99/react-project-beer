4ff7607e4a8ac8e26322e439d28a298871dda7a1
Airidas <airidassien64@gmail.com>
2022-12-08 14:59:36 +0000

    got the search bar on the page, but not currently working

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import beers from './Data/Data';
 import './App.css';
 import Main from './Components/Main/main';
 import React from 'react';
+import NavBar from './Components/NavBar/navBar';
+import { useState } from 'react';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -12,13 +14,14 @@ function App() {
   };
   const filtered = beers.filter((beer) => {
     const beerLowerCase = beer.name.toLowerCase();
-    return beerLowerCase.includes(searchTerm) && users;
+    return beerLowerCase.includes(searchTerm) && beer;
   });
 
   return (
     <div>
     <h1>Beer api</h1>
     <div>
+    < NavBar handleInput={handleInput} searchTerm={searchTerm} />
     <Main beers={beers}/>
     </div>
     </div>
--- src/Components/NavBar/navBar.jsx
+++ src/Components/NavBar/navBar.jsx
@@ -2,3 +2,17 @@ import "./navBar.scss"
 import SearchBox from "../SearchBox/SearchBox"
 
 
+const NavBar = ({handleInput, searchTerm}) => {
+return(
+<div>
+    <SearchBox handleInput={handleInput} searchTerm={searchTerm} />
+</div>
+
+
+
+)
+
+
+
+}
+export default NavBar;
