b052a361a4dd045ce904b706dbe2c78a4a149b58
Airidas <airidassien64@gmail.com>
2022-12-13 18:53:01 +0000

    installed jest, and created a test which passed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+it("should display the heading with the correct text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/Beer API/i);
+  expect(heading).toBeInTheDocument();
 });
